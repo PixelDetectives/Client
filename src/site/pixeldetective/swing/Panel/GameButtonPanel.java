da4e4e401a3ea1a49e17d87a9f7950f5476ca25c
whdudgms <whdudgms123@naver.com>
2024-06-27 19:07:49 +0900

    LobbyFrame에서 방만들기 버튼을 누르면  MakeRoomFrame으로 전환

--- src/site/pixeldetective/swing/Panel/GameButtonPanel.java
+++ src/site/pixeldetective/swing/Panel/GameButtonPanel.java
@@ -1,5 +1,6 @@
 package site.pixeldetective.swing.Panel;
 
+import site.pixeldetective.swing.Frame.LobbyFrame;
 import site.pixeldetective.swing.Frame.MakeRoomFrame;
 
 import javax.swing.*;
@@ -8,6 +9,9 @@ import java.awt.event.ActionEvent;
 
 // LobbyFrame >> LobbyPanel >> GameButtonPanel 방만들기 빠른매칭 버튼 클릭
 public class GameButtonPanel extends JPanel {
+
+    public  JButton button1;
+    public  JButton button2;
     public GameButtonPanel() {
         setLayout(new GridBagLayout()); // 더 유연한 레이아웃 관리
 
@@ -21,7 +25,7 @@ public class GameButtonPanel extends JPanel {
 
 
         // 첫 번째 버튼 생성 및 설정
-        JButton button1 = new JButton("방만들기");
+        this.button1 = new JButton("방만들기");
         button1.setPreferredSize(new Dimension(297, 101));
         button1.setFont(buttonFont);
         gbc.gridx = 0; // 첫 번째 버튼의 열 인덱스
@@ -29,7 +33,7 @@ public class GameButtonPanel extends JPanel {
         add(button1, gbc);
 
         // 두 번째 버튼 생성 및 설정
-        JButton button2 = new JButton("빠른시작");
+        this.button2 = new JButton("빠른시작");
         button2.setPreferredSize(new Dimension(297, 101));
         button2.setFont(buttonFont);
         gbc.gridx = 1; // 두 번째 버튼의 열 인덱스
@@ -37,19 +41,6 @@ public class GameButtonPanel extends JPanel {
         add(button2, gbc);
 
 
-        //방만들기 버튼 Frame전환 기능이 만들어져야함  !!!
-        button1.addActionListener(new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("button1");
-                // LobbyFrame > MakeRoomFrame
-
-                new MakeRoomFrame(); // 새로운 프레임 열기
-
-
-
-            }
-        });
 
 
         //랜덤으로 현재 만들어진 방들 중에서 방에 입장할 수 있어야 함
