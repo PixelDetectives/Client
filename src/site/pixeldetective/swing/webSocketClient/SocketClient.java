8a9031d8328fd8cd152bb92846b4ddcbd9c18b2c
whdudgms <108641428+whdudgms@users.noreply.github.com>
2024-06-28 11:46:38 +0900

    socket class create

---
+++ src/site/pixeldetective/swing/webSocketClient/SocketClient.java
@@ -0,0 +1,118 @@
+package site.pixeldetective.swing.webSocketClient;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+import org.json.JSONObject;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.concurrent.CompletableFuture;
+
+public class SocketClient extends WebSocketClient {
+
+    private static final String SERVER_URI = "ws://localhost:9001";
+    private CompletableFuture<String> responseFuture;
+
+    public SocketClient() throws URISyntaxException {
+        super(new URI(SERVER_URI));
+    }
+
+    @Override
+    public void onOpen(ServerHandshake handshake) {
+        System.out.println("Connected to WebSocket server");
+    }
+
+    @Override
+    public void onMessage(String message) {
+        System.out.println("Received message: " + message);
+        if (responseFuture != null) {
+            responseFuture.complete(message);
+        }
+    }
+
+    @Override
+    public void onClose(int code, String reason, boolean remote) {
+        System.out.println("Disconnected from WebSocket server");
+        if (responseFuture != null) {
+            responseFuture.completeExceptionally(new Exception("WebSocket connection closed"));
+        }
+    }
+
+    @Override
+    public void onError(Exception ex) {
+        System.out.println("WebSocket error: " + ex.getMessage());
+        if (responseFuture != null) {
+            responseFuture.completeExceptionally(ex);
+        }
+    }
+
+    private void sendMessage(JSONObject request) throws Exception {
+        if (isOpen()) {
+            send(request.toString());
+        } else {
+            throw new Exception("WebSocket is not connected");
+        }
+    }
+
+    public String sendHello() throws Exception {
+        JSONObject request = new JSONObject();
+        request.put("command", "hello");
+        responseFuture = new CompletableFuture<>();
+        sendMessage(request);
+        return responseFuture.get();
+    }
+
+    public void sendGetUserCount() throws Exception {
+        JSONObject request = new JSONObject();
+        request.put("command", "getUserCount");
+        sendMessage(request);
+    }
+
+    public String sendChatMessage(String nickname, String message) throws Exception {
+        JSONObject request = new JSONObject();
+        request.put("command", "sendChat");
+        request.put("nickname", nickname);
+        request.put("message", message);
+        responseFuture = new CompletableFuture<>();
+        sendMessage(request);
+        return responseFuture.get();
+    }
+
+    public void getCurrentUserList() throws Exception {
+        JSONObject request = new JSONObject();
+        request.put("command", "getCurrentUserList");
+        sendMessage(request);
+    }
+
+    public void quickMatching() throws Exception {
+        JSONObject request = new JSONObject();
+        request.put("command", "quickMatching");
+        sendMessage(request);
+    }
+
+    public void cancelMatching() throws Exception {
+        JSONObject request = new JSONObject();
+        request.put("command", "cancleMatching");
+        sendMessage(request);
+    }
+
+    public void createRoom(String roomName, int difficulty) throws Exception {
+        JSONObject request = new JSONObject();
+        request.put("command", "roomCreate");
+        request.put("r_name", roomName);
+        request.put("r_difficulty", difficulty);
+        sendMessage(request);
+    }
+
+    public void getCurrentRoomList() throws Exception {
+        JSONObject request = new JSONObject();
+        request.put("command", "getCurrentRoomList");
+        sendMessage(request);
+    }
+
+    public void deleteRoom() throws Exception {
+        JSONObject request = new JSONObject();
+        request.put("command", "roomDelete");
+        sendMessage(request);
+    }
+}
